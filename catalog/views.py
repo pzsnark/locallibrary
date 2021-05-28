5fc090fea0084786644ad22f37c38a09aa94e74b
PzSnark <pzsnark@gmail.com>
2021-05-28 19:52:04 +0400

    initial

---
+++ catalog/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import *
+
+
+def index(request):
+    """
+    Функция отображения для домашней страницы сайта.
+    """
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)  # получаем значение num_visits из сессии и назначает 0, если оно не установлено ранее
+    request.session['num_visits'] = num_visits + 1
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books,
+                 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available,
+                 'num_authors': num_authors,
+                 'num_visits': num_visits},
+    )
+
+
+# https://docs.djangoproject.com/en/1.10/topics/class-based-views/generic-display/
+class BookListView(ListView):
+    model = Book
+    paginate_by = 10
+    # context_object_name = 'my_book_list'  # ваше собственное имя переменной контекста в шаблоне
+    # queryset = Book.objects.filter(title__icontains='war')[:5]  # Получение 5 книг, содержащих слово 'war' в заголовке
+    # template_name = 'books/my_arbitrary_template_name_list.html'  # Определение имени вашего шаблона и его расположения
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='war')[:5]  # Получить 5 книг, содержащих 'war' в заголовке
+
+    # def get_context_data(self, **kwargs):
+    #     # В первую очередь получаем базовую реализацию контекста
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     # Добавляем новую переменную к контексту и инициализируем её некоторым значением
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+
+
+class BookDetailView(DetailView):
+    model = Book
+
+
+class AuthorListView(ListView):
+    model = Author
+    paginate_by = 20
+
+
+class AuthorDetailView(DetailView):
+    model = Author
+
+
+class LoanedBooksByUserListView(LoginRequiredMixin, ListView):
+
+    model = BookInstance
+    template_name = 'catalog/bookinstance_list_borrowed_user.html'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
